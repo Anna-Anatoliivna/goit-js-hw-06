6b5e45ab8bde7239514b19db168c4d60470ebb4e
Anna <iaanya27@gmail.com>
2024-05-25 09:45:26 +0300

    added task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', myForm);
 
+function myForm(evt) {
+    evt.preventDefault();
+    let isValid = false;
+    let inputArr = document.querySelectorAll('login-form input')
+    inputArr = Arr.from(inputArr);
+    inputArr.forEach(input => {
+        if (input.value) {
+            isValid = true;
+        } else {
+            isValid = false;
+        }        
+    });
+    if (!isValid) {
+        alert('All form fields must be filled in')
+    }
+    let UserEmail = formEl.elements[0].value;
+    let UserPassword = formEl.elements[1].value;
+    let formObj = {
+        email: UserEmail,
+        password: UserPassword,
+    };
+    formEl.reset();
+    return console.log(formObj);
+}
