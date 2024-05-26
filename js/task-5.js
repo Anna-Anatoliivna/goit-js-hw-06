de8ccbe55d90e8b1d6c335ad9f10b06edb5ce0b9
Anna <iaanya27@gmail.com>
2024-05-26 13:05:24 +0300

    added task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector('.widget');
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", onBtnElClick);
+function onBtnElClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
